a8027aaffcecc3a35367225524d50a5392eda264
PetroSenyo <45631420+PetroSenyo@users.noreply.github.com>
2024-01-24 00:34:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5062,6 +5062,8 @@ class na extends We {
 
     addEvents() {
         super.addEvents(), this.nextButton.addEventListener("click", () => T.scroll.toNext())
+        super.addEvents(), this.nextButton.addEventListener("click", () => T.scroll.toNext())
+        super.addEvents(), this.nextButton.addEventListener("click", () => T.scroll.toNext())
     }
 
     onResize() {
