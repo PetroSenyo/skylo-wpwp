3a30259282cc2ec3e090fa895abfd9b675e89280
PetroSenyo <45631420+PetroSenyo@users.noreply.github.com>
2024-08-06 17:38:49 +0300

    Update main-2.js

--- main-2.js
+++ main-2.js
@@ -5102,14 +5102,14 @@ class kk extends We {
     }
 
     onScroll(e) {
-        if (!!T.gl && ((e.id !== "GLOBE-STEP-11" || e.id !== "TRUCK-STEP-3") && (this.el.style.opacity = 0), e.id === "TRUCK-STEP-3" && T.gl.camera.center.copy(K.w < H.BREAKPOINTS.MD ? T.gl.camera.centerDefaults.mobile : T.gl.camera.centerDefaults.dekstop), e.id === "TRUCK-STEP-3")) {
+        if (!!T.gl && ((e.id !== "GLOBE-STEP-11" || e.id !== "GLOBE-STEP-2") && (this.el.style.opacity = 0), e.id === "GLOBE-STEP-2" && T.gl.camera.center.copy(K.w < H.BREAKPOINTS.MD ? T.gl.camera.centerDefaults.mobile : T.gl.camera.centerDefaults.dekstop), e.id === "GLOBE-STEP-2")) {
             if (!e.inView) return;
             const t = I(e.progress, 0, 1, 1, 3);
             T.gl.setOpacity(I(e.progress, 0, .5, 1, 0)), W(this.text, I(e.progress, 0, 1, 0, -300)), this.text.style.opacity = fe(I(e.progress, 0, .5, 1, 0), 0, 1), this.markers.style.opacity = fe(I(e.progress, 0, .5, 1, 0), 0, 1)
         }
         console.log(77777);
 
-        if (e.id === "TRUCK-STEP-3") {
+        if (e.id === "GLOBE-STEP-2") {
             if (!e.inView) return;
             const t = I(e.progress, 0, 1, 1, 3);
             T.gl.setOpacity(I(e.progress, 0, .5, 1, 0)), W(this.text, I(e.progress, 0, 1, 0, -300)), this.text.style.opacity = fe(I(e.progress, 0, .5, 1, 0), 0, 1), this.markers.style.opacity = fe(I(e.progress, 0, .5, 1, 0), 0, 1)
